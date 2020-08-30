f8e927f88c18cc6fa6fe202f1a67b957f1b4083e
Beshayr <beshayr37@gmail.com>
2020-08-29 21:17:40 -0500

    adding all files

---
+++ questions.js
@@ -0,0 +1,49 @@
+var questions = [
+  {
+    title: "Commonly used data types DO NOT Include:",
+    choices: ["1.strings", "2.booleans", "3.alerts", "4.numbers"],
+    answer: "3.alerts",
+  },
+
+  {
+    title: "The Condition in an if / else statement is enclosed within ____ .",
+    choices: [
+      "1.qoutes",
+      "2.curly bracktes",
+      "3.parenthese",
+      "4.square brackets",
+    ],
+    answer: "2.curly bracktes",
+  },
+
+  {
+    title: "A very useful tool used during development and debugging for printing for printing contents to the debugger is: ",
+    choices: ["1.javascript", "2.termial/bash", "3.for loop", "4.console log"],
+    answer: "4.console log",
+  },
+  
+
+  {
+    title: "Arrays on JavaScript can be used to store",
+    choices: ["1.numbers and strings", "2.other arrays", "3.booleans", "4.all of the above"],
+    answer: "4.all of the above",
+  },
+
+  {
+    title: "String values must be enclosed within _____ when being assigned to variables.",
+    choices: [
+      "1.Commas",
+      "2.curly bracktes",
+      "3.qoutes",
+      "4.parenthese",
+    ],
+    answer: "3.qoutes",
+  },
+
+  {
+    title: "Arrays on JavaScript can be used to store",
+    choices: ["1.numbers and strings", "2.other arrays", "3.booleans", "4.all of the above"],
+    answer: "4.all of the above",
+  },
+ 
+];
